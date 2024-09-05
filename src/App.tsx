c64024c89fa18b54686ac786e0394ccab1375903
chreazy <vladekk.13@yandex.ru>
2024-09-05 22:51:01 +0300

    workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+
 import './App.css'
 import { TonConnectButton } from '@tonconnect/ui-react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const dappMetadata = {
-    manifestUrl:
-      'https://pastebin.com/raw/zQN5PYhE',
-  };
 
   return (
     <>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
-const manifest = "https://pastebin.com/raw/zQN5PYhE";
+
+const manifest = 'https://chreazy.github.io/bluewater/public/ton-connect-manifest.json';
+
 createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifest}>
     <App />
